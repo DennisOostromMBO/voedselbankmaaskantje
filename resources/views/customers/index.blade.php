050af497e1e4164a0f79147c9b860a8a88459100
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-06-27 14:33:53 +0200

    index customers werkt

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Klanten</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['public/css/app.css', 'resources/js/app.js'])
     <link href="https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&display=swap" rel="stylesheet">
 </head>
 <body class="bg-gray-50 font-sans" style="font-family: 'Noto Color Emoji', sans-serif;">
