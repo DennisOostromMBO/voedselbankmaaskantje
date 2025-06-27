f46d2fe0f29ab821b5f8b0d0855cfc3d781c31ec
mahdi2662 <madialkhalidi83@gmail.com>
2025-06-27 16:05:29 +0200

    heb de navbar in de stocks index gezet en de stocks route in de navbar

--- resources/views/Stocks/Index.blade.php
+++ resources/views/Stocks/Index.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
+    @include('components.navbar')
     <main class="main-content">
         <div class="container">
             <div class="card">
