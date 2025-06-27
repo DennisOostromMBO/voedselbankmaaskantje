4dc3aeb140542d7a3abe0bd08ef7dcf7d398f518
Dennis Oostrom <336914@student.mboutrecht.nl>
2025-06-27 14:21:47 +0200

    routes opvolgorde

--- routes/Customer.php
+++ routes/Customer.php
@@ -3,8 +3,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
  
 Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
-Route::delete('/customers/{id}', [CustomerController::class, 'destroy'])->name('customers.destroy');
-Route::get('/customers/{id}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
-Route::put('/customers/{id}', [CustomerController::class, 'update'])->name('customers.update');
 Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
 Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
+Route::get('/customers/{id}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
+Route::put('/customers/{id}', [CustomerController::class, 'update'])->name('customers.update');
+Route::delete('/customers/{id}', [CustomerController::class, 'destroy'])->name('customers.destroy');
