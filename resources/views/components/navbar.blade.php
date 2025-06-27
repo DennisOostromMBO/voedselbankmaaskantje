1184db8f00ce42e8c8c4aad5af02cabfc4901577
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-06-27 16:10:34 +0200

    navbar werkt nu

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,7 +4,7 @@
         <ul class="nav-links flex gap-4 mt-4 md:mt-0">
             <li><a href="/" class="nav-link active flex items-center gap-2 text-white"><i class="fas fa-home"></i> Dashboard</a></li>
             <li><a href="/customers" class="nav-link flex items-center gap-2 text-white"><i class="fas fa-users"></i> Customers</a></li>
-            <li><a href="#" class="nav-link flex items-center gap-2 text-white"><i class="fas fa-box"></i> Food Parcels</a></li>
+            <li><a href="/food-parcels" class="nav-link flex items-center gap-2 text-white"><i class="fas fa-box"></i> Food Parcels</a></li>
             <li><a href="/stocks" class="nav-link flex items-center gap-2 text-white"><i class="fas fa-warehouse"></i> Stock</a></li>
             <li><a href="/suppliers" class="nav-link flex items-center gap-2 text-white"><i class="fas fa-truck"></i> Suppliers</a></li>
             <li><a href="#" class="nav-link flex items-center gap-2 text-white"><i class="fas fa-user-circle"></i> Profile</a></li>
